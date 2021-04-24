d1787e20220e6a0aaee02b9fbbdef99c1cede1ef
sirineni vamsi <60559764+vsirineni@users.noreply.github.com>
2021-04-24 14:31:56 -0500

    update

--- tests/cityscoop_testsuit.py
+++ tests/cityscoop_testsuit.py
@@ -1,15 +1,19 @@
 import unittest
 from tests.cityscoop_login import cityscoop_login
 from tests.cityscoop_search import cityscoop_search
+from tests.cityscoop_profile import cityscoop_profile
+from tests.cityscoop_about import cityscoop_about
 
 
 
 login = unittest.TestLoader().loadTestsFromTestCase(cityscoop_login)
 search = unittest.TestLoader().loadTestsFromTestCase(cityscoop_search)
+profile = unittest.TestLoader().loadTestsFromTestCase(cityscoop_profile)
+about = unittest.TestLoader().loadTestsFromTestCase(cityscoop_about)
 
 
 
 test_suite = unittest.TestSuite(
-    [login, search])
+     [login, search, profile, about])
 
 unittest.TextTestRunner().run(test_suite)
